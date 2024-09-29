6cd2a5a8354286af67221afbdef3e8d65aa0368f
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-09-29 17:22:36 -0600

    challenge 1 solved

--- workbooks-challenges/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ workbooks-challenges/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,13 @@
 public class YourInitials {
     public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        // Instructions for this workbook are on Learn the Part (See the Udemy Video:
+        // Your Initials to access the link).
+        System.out.println("JJJJJ   CCC ");
+        System.out.println("  J    C   C");
+        System.out.println("  J    C    ");
+        System.out.println("  J    C    ");
+        System.out.println("  J    C    ");
+        System.out.println("J J    C   C");
+        System.out.println(" JJ     CCC ");
     }
 }
