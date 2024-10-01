61ef17fc68303f2d82f9bf961269ea7bd89ba919
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-10-01 10:24:36 -0600

    section two - character data type

---
+++ section-two/Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+    public static void main(String[] args) {
+        char potions = 'A';
+        char arithmancy = 'B';
+        char charms = 'C';
+
+        System.out.println(potions);
+        System.out.println(arithmancy);
+        System.out.println(charms);
+    }
+}
