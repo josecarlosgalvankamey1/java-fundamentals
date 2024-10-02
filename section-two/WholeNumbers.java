b5a3a677b5924aad2c75e2531a5c3de38b7c47c3
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-10-02 06:19:17 -0600

    section two - the long data type

---
+++ section-two/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        // int globalPopulation = 8_000_000_000; -> Error.
+        long globalPopulation = 8000000000L;
+        long dailyGoogleSearches = 8500000000L;
+        System.out.println(globalPopulation);
+        System.out.println(dailyGoogleSearches);
+    }
+}
