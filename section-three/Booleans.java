f0a3f7298a5880d6f663d76c0b8704c9c27acc2f
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-10-14 06:51:53 -0600

    section three - comparison operators

---
+++ section-three/Booleans.java
@@ -0,0 +1,9 @@
+public class Booleans {
+    public static void main(String[] args) {
+        boolean bol1 = true;
+        boolean bol2 = false;
+
+        System.out.println(bol1);
+        System.out.println(bol2);
+    }
+}
