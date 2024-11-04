045ccd1f56bdeb590947d12d25f121cd9654bbb5
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-03 21:16:04 -0600

    workbook 5.3 solved

--- workbooks-challenges/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ workbooks-challenges/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -5,6 +5,13 @@ public class CountingTool {
         Scanner scan = new Scanner(System.in);
 
         // See Learn the Part for the instructions.
+        System.out.print("Hi Timmy! Choose a number to count to: ");
+        int timmyNumber = scan.nextInt();
+        System.out.println("Great! Here's how it's done");
+
+        for (int i = 1; i <= timmyNumber; i++) {
+            System.out.print(i + " ");
+        }
 
         scan.close();
     }
