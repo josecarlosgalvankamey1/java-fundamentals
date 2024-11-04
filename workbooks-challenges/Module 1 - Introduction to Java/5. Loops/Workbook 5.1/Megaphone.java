34dc662af60636b7c3e0d1d71ab5367afe587c06
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-03 19:16:58 -0600

    workbook 5.1 solved

--- workbooks-challenges/Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
+++ workbooks-challenges/Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
@@ -1,6 +1,13 @@
 public class Megaphone {
     public static void main(String[] args) {
         // See Learn the Part for the instructions.
+        for (int i = 0; i < 11; i++) {
+            System.out.println("If Java was easy, they would call it Python!");
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ". If Java was easy, they would call it Python!");
+        }
     }
-    
+
 }
