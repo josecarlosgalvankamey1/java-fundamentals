50ef0fed84ddaa6b4225e2025ed2dd960384ecac
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-05 18:08:18 -0600

    section six - arrays

---
+++ section-six/IntroToArrays.java
@@ -0,0 +1,10 @@
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = { "Mercia", "Wessex", "Northumbria", "East Anglia" };
+
+        System.out.println("Kingdom 0: " + kingdoms[0]);
+        System.out.println("Kingdom 1: " + kingdoms[1]);
+        System.out.println("Kingdom 2: " + kingdoms[2]);
+        System.out.println("Kingdom 3: " + kingdoms[3]);
+    }
+}
