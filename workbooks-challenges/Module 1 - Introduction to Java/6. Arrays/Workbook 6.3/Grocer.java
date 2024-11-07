418288eea9762365370b25ee750e8f9272251c2c
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-07 14:11:39 -0600

    workbook 6.3 solved

--- workbooks-challenges/Module 1 - Introduction to Java/6. Arrays/Workbook 6.3/Grocer.java
+++ workbooks-challenges/Module 1 - Introduction to Java/6. Arrays/Workbook 6.3/Grocer.java
@@ -1,10 +1,21 @@
 public class Grocer {
     public static void main(String[] args) {
-        
+
         // Instructions for this workbook are on Learn the Part (Workbook 6.3).
 
+        String[] products = { "apples", "bananas", "candy", "chocolate", "coffee", "tea" };
+
         System.out.println("\nDo you sell coffee?");
-        System.out.println("\nWe have that in aisle: <index>");
-    
+
+        int index = -1;
+        for (int i = 0; i < products.length; ++i) {
+            if (products[i].equals("coffee")) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println("\nWe have that in aisle: " + index);
+
     }
 }
