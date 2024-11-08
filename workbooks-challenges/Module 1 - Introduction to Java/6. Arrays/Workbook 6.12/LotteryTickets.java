9390ca8bf462a271023e60bc1bf91341f2288f6b
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-07 18:19:27 -0600

    workbook 6.12 solved

--- workbooks-challenges/Module 1 - Introduction to Java/6. Arrays/Workbook 6.12/LotteryTickets.java
+++ workbooks-challenges/Module 1 - Introduction to Java/6. Arrays/Workbook 6.12/LotteryTickets.java
@@ -1,18 +1,39 @@
-import java.util.Arrays;
 
 public class LotteryTickets {
     public static void main(String[] args) {
-        
+
         // Instructions for this workbook are on Learn the Part (workbook 6.12)
-        int[] ticket = {34, 43, 45, 65, 21, 54};
-        int[] ticket2;
+        int[] ticket = { 34, 43, 45, 65, 21, 54 };
+        int[] ticket2 = ticket.clone();
+
+        ticket2[2] = 54;
 
         System.out.print("Ticket 1 numbers: ");
-        //Task 3 - Call printTicketNumbers for int[] ticket.   
+        // Task 3 - Call printTicketNumbers for int[] ticket.
+        printTicketNumbers(ticket);
 
         System.out.print("Ticket 2 numbers: ");
-        //Task 3 - Call printTicketNumbers for int[] ticket2.   
-    } 
-    
-}
+        // Task 3 - Call printTicketNumbers for int[] ticket2.
+        printTicketNumbers(ticket2);
+    }
 
+    /**
+     * Function name -- printTicketNumbers
+     * 
+     * @param ticket ( int[] )
+     *
+     *               Inside the function:
+     *               1. Use a loop to print the numbers in one line.
+     *               System.out.print(ticket[i] + " ");
+     *
+     *               2. After the loop, print two new lines.
+     *               System.out.print("\n\n");
+     *
+     */
+    public static void printTicketNumbers(int[] tickets) {
+        for (int i = 0; i < tickets.length; ++i) {
+            System.out.print(tickets[i] + " ");
+        }
+        System.out.println("\n\n");
+    }
+}
