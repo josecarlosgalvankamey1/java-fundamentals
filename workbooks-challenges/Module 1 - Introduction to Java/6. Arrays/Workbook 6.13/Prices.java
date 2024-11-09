377a37174114512b63dbc1a1a11a93243e583836
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-08 21:42:40 -0600

    workbook 6.13 revisited

--- workbooks-challenges/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ workbooks-challenges/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -7,8 +7,30 @@ public class Prices {
                 { 8.99, 7.99, 9.49, 9.99, 10.99 }
         };
 
-        System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2]);
-        System.out.println("Beverage: " + prices[1][0] + " " + prices[1][1] + " " + prices[1][2]);
-        System.out.println("Cereals: " + prices[2][0] + " " + prices[2][1] + " " + prices[2][2]);
+        // System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " +
+        // prices[0][2]);
+        // System.out.println("Beverage: " + prices[1][0] + " " + prices[1][1] + " " +
+        // prices[1][2]);
+        // System.out.println("Cereals: " + prices[2][0] + " " + prices[2][1] + " " +
+        // prices[2][2]);
+
+        for (int i = 0; i < prices.length; ++i) {
+            switch (i) {
+                case 0:
+                    System.out.print("Baking: ");
+                    break;
+                case 1:
+                    System.out.print("Beverage: ");
+                    break;
+                case 2:
+                    System.out.print("Cereals: ");
+                    break;
+            }
+
+            for (int j = 0; j < prices[i].length; ++j) {
+                System.out.print(prices[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
     }
 }
