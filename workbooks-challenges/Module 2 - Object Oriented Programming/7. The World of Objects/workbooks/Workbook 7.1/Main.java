55bc4ac1335f5d96871486ea24eca372cd009d1c
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-12 18:04:00 -0600

    workbook 7.1 solved

--- workbooks-challenges/Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
@@ -1,3 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Person carlos = new Person();
+
+        carlos.name = "Carlos";
+        carlos.nationality = "Mexican";
+        carlos.dateOfBirth = "26/08/2001";
+        carlos.passport = new String[] { carlos.name, carlos.nationality, carlos.dateOfBirth };
+        carlos.seatNumber = 9;
+
+        System.out.println(carlos.name);
+        System.out.println(carlos.nationality);
+        System.out.println(carlos.dateOfBirth);
+        System.out.println(Arrays.toString(carlos.passport));
+        System.out.println(carlos.seatNumber);
+    }
 }
