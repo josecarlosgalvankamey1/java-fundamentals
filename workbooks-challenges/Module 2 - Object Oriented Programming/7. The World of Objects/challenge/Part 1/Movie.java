032efd215c9dda4b51885ee3e7e31351c5578841
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-19 16:10:22 -0600

    section seven - challenge part 1 solved

--- workbooks-challenges/Module 2 - Object Oriented Programming/7. The World of Objects/challenge/Part 1/Movie.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/7. The World of Objects/challenge/Part 1/Movie.java
@@ -1,5 +1,23 @@
 public class Movie {
 
-    
+    private String name;
+    private String format;
+    private double rating;
 
+    public Movie(String name, String format, double rating) {
+        this.name = name;
+        this.format = format;
+        this.rating = rating;
+    }
+
+    public Movie(Movie source) {
+        this.name = source.name;
+        this.format = source.format;
+        this.rating = source.rating;
+    }
+
+    @Override
+    public String toString() {
+        return this.rating + "\t" + this.format + "\t\t" + this.name + "";
+    }
 }
