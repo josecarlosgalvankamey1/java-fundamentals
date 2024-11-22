7fdcc0c4614302923bcacb1a2766c1920eaad90a
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-22 17:42:35 -0600

    compile-time exceptions

--- workbooks-challenges/Module 2 - Object Oriented Programming/10. Exception Handling/Lessons/checked-exceptions/ExampleOne.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/10. Exception Handling/Lessons/checked-exceptions/ExampleOne.java
@@ -1,6 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ExampleOne {
     public static void main(String[] args) {
-
+        // greetings.txt
+        try {
+            FileInputStream fis = new FileInputStream("greetings.txt");
+            Scanner scanner = new Scanner(fis);
+            System.out.println(scanner.nextLine());
+            scanner.close();
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println(fileNotFoundException.getMessage());
+        } finally {
+            System.out.println("Process completed");
+        }
     }
-    
+
 }
--- workbooks-challenges/Module 2 - Object Oriented Programming/10. Exception Handling/Lessons/checked-exceptions/ExampleTwo.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/10. Exception Handling/Lessons/checked-exceptions/ExampleTwo.java
@@ -1,12 +1,22 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ExampleTwo {
     public static void main(String[] args) {
-
+        try {
+            readFile("greetings.txt");
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println(fileNotFoundException.getMessage());
+        } finally {
+            System.out.println("Process completed");
+        }
     }
-    
-    public static void readFile(String fileName) {
-        // FileInputStream fis = new FileInputStream("greetings.txt");
-        // Scanner scanner = new Scanner(fis);
-        // System.out.println(scanner.nextLine());
-        // scanner.close();
+
+    public static void readFile(String fileName) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Scanner scanner = new Scanner(fis);
+        System.out.println(scanner.nextLine());
+        scanner.close();
     }
 }
