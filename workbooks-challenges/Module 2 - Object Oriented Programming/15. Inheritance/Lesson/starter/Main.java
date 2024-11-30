17fc0b1960a7d9789748e1c60b93f913bcf8fd35
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-11-30 12:07:03 -0600

    Inheritance - the super constructor

--- workbooks-challenges/Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
@@ -1,4 +1,5 @@
 import product.Pants;
+import product.Product;
 import product.Shirt;
 import product.Shirt.Size;
 
@@ -6,20 +7,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Shirt shirt = new Shirt();
-        shirt.setSize(Size.MEDIUM);
-        shirt.setColor("RED");
-        shirt.setPrice(4.99);
-        shirt.setBrand("NIKE");
+        Shirt shirt = new Shirt(3.99, "RED", "NIKE", Size.MEDIUM);
         shirt.fold();
 
-        Pants pants = new Pants();
-        pants.setColor("BLACK");
-        pants.setWaist(32);
-        pants.setLength(32);
-        pants.setPrice(7.99);
-        pants.setBrand("LEVI'S");
+        Pants pants = new Pants(7.99, "BLACK", "LEVI'S", 32, 32);
         pants.fold();
+
+        productStore(pants);
+        productStore(shirt);
+
+        pantStore(pants);
+        shirtStore(shirt);
     }
 
+    public static void productStore(Product product) {
+        System.out.println(
+                "\nThank you for purchasing the " + product.getBrand() + " " + product.getClass().getSimpleName()
+                        + " Your total comes to "
+                        + product.getPrice());
+    }
+
+    public static void pantStore(Pants pants) {
+        System.out.println("\nThank you for purchasing the " + pants.getBrand() + " Pants. " + "Your total comes to "
+                + pants.getPrice());
+    }
+
+    public static void shirtStore(Shirt shirt) {
+        System.out.println("\nThank you for purchasing the " + shirt.getBrand() + " Shirt. " + "Your total comes to "
+                + shirt.getPrice());
+    }
 }
