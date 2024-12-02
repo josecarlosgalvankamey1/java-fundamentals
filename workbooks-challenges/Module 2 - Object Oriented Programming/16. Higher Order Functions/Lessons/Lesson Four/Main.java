8d61e4ef6bee6fbdc90206a262a74ddddb261ac1
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-02 13:10:56 -0600

    Higher order functions - stream operations

--- workbooks-challenges/Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Four/Main.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Four/Main.java
@@ -6,5 +6,12 @@ public class Main {
 
         List<Double> prices = Arrays.asList(341.67, 209.32, 88.41, 269.99, 68.49, 499.99, 28.12, 354.38);
 
+        prices.stream()
+                .filter(price -> price > 100)
+                .map(x -> x - 20)
+                .sorted((x, y) -> x.compareTo(y))
+                .map(x -> "$" + String.valueOf(x))
+                .forEach(price -> System.out.println(price));
+
     }
 }
