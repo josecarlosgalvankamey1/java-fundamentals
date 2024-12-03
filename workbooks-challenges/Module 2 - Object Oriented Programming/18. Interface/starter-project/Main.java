3cb0f65f1e59a4ee2ff0a77f8242a36274c5654d
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-03 13:06:47 -0600

    Three layer design

--- workbooks-challenges/Module 2 - Object Oriented Programming/18. Interface/starter-project/Main.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/18. Interface/starter-project/Main.java
@@ -5,22 +5,34 @@ import java.util.List;
 import pojo.Account;
 import pojo.Checking;
 import pojo.Credit;
+import repository.AccountRepository;
+import service.CheckingService;
+import service.CreditService;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        AccountRepository repository = new AccountRepository();
+        CheckingService checkingService = new CheckingService(repository);
+        CreditService creditService = new CreditService(repository);
+
         // Assume these were obtained from user input.
         List<Account> accounts = Arrays.asList(
-            new Checking("A1234B", new BigDecimal("500.00")),
-            new Checking("E3456F", new BigDecimal("300.50")),
-            new Checking("I5678J", new BigDecimal("100.25")),
-            new Credit("A1534B", new BigDecimal("0.50")),
-            new Credit("G4567H", new BigDecimal("200.00"))
-        );
+                new Checking("A1234B", new BigDecimal("500.00")),
+                new Checking("E3456F", new BigDecimal("300.50")),
+                new Checking("I5678J", new BigDecimal("100.25")),
+                new Credit("A1534B", new BigDecimal("0.50")),
+                new Credit("G4567H", new BigDecimal("200.00")));
 
+        accounts.forEach(account -> {
+            if (account instanceof Credit) {
+                creditService.createAccount((Credit) account);
+            } else {
+                checkingService.createAccount((Checking) account);
+            }
+        });
 
+        Account account = repository.retrieveAccount("A1234B");
     }
-
-
 }
--- workbooks-challenges/Module 2 - Object Oriented Programming/18. Interface/starter-project/service/CheckingService.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/18. Interface/starter-project/service/CheckingService.java
@@ -1,8 +1,29 @@
 package service;
 
+import pojo.Checking;
+import repository.AccountRepository;
 
 public class CheckingService {
-        
 
+    AccountRepository accountRepository;
 
+    public CheckingService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public void createAccount(Checking account) {
+        this.accountRepository.createAccount(account.clone());
+    }
+
+    public Checking retrieveAccount(String id) {
+        return (Checking) this.accountRepository.retrieveAccount(id).clone();
+    }
+
+    public void updateAccount(Checking account) {
+        this.accountRepository.updateAccount(account);
+    }
+
+    public void deleteAccount(String id) {
+        this.accountRepository.deleteAccount(id);
+    }
 }
