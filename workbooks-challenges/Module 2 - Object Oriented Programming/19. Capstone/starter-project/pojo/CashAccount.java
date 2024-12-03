625ba82c52398f5bffa73afaa62d4b7abf426ffc
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-03 16:58:34 -0600

    Module 2 capstone project part 1 solved

--- workbooks-challenges/Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/CashAccount.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/CashAccount.java
@@ -1,6 +1,26 @@
 package pojo;
 
-public class CashAccount {
+import java.math.BigDecimal;
 
+public class CashAccount extends TradeAccount {
 
+    private BigDecimal cashBalance;
+
+    public CashAccount(String id, BigDecimal cashBalalance) {
+        super(id);
+        this.cashBalance = cashBalalance;
+    }
+
+    public BigDecimal getCashBalance() {
+        return this.cashBalance;
+    }
+
+    public void setCashBalance(BigDecimal cashBalance) {
+        this.cashBalance = cashBalance;
+    }
+
+    @Override
+    public CashAccount clone() {
+        return new CashAccount(super.getId(), this.getCashBalance());
+    }
 }
