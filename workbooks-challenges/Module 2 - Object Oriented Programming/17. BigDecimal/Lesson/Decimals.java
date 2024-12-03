c6e0496dc158e2c80555feee0ac28adfcdda476f
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-03 12:33:10 -0600

    BigDecimal

--- workbooks-challenges/Module 2 - Object Oriented Programming/17. BigDecimal/Lesson/Decimals.java
+++ workbooks-challenges/Module 2 - Object Oriented Programming/17. BigDecimal/Lesson/Decimals.java
@@ -1,14 +1,24 @@
+import java.math.BigDecimal;
+
 public class Decimals {
     public static void main(String[] args) {
 
-        double x = 0.1;
+        // double x = 0.1;
 
-        System.out.println(x);
+        // System.out.println(x);
 
-        double y = 0.2;
-        double z = x + y;
-        
-        System.out.println(z);
+        // double y = 0.2;
+
+        // System.out.println(y);
+
+        // double z = x + y;
+
+        // System.out.println(z);
+
+        BigDecimal x = new BigDecimal("0.1");
+        BigDecimal y = new BigDecimal("0.2");
+
+        System.out.println(x.add(y));
 
     }
 
